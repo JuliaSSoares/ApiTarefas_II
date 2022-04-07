73c30027d585d14dac04a6d2cce7e2651e0d25ba
Julia Soares <jusoaresbtu@gmail.com>
2022-04-07 16:48:21 -0300

    add ServiceCollectionExtensions

---
+++ ApiTarefas_II/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using static ApiTarefas_II.Data.TarefaContext;
+
+namespace TarefasApi.Extensions
+{
+    public static class ServiceCollectionsExtensions
+    {
+        public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddScoped<GetConnection>(sp =>
+            async () =>
+            {
+                var connection = new SqlConnection(connectionString);//criando uma nova conecção
+                await connection.OpenAsync();
+                return connection;
+            });
+            return builder;
+        }
+    }
+}
+//Pode-se então usar o método de extesão na classe program;
